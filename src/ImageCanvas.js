8baf2db9b8593dc6c0ac91325f51d545342b38bb
unknown <a-kaname@weldh.local>
2020-06-29 16:37:20 +0900

    canvas操作パッケージ追加

--- src/ImageCanvas.js
+++ src/ImageCanvas.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fabric } from 'fabric';
 
 const ImageCanvas = (props) => {
-  const { uploadImage } = props
+  const { uploadImage, canvas, updateCanvas } = props
+
+  // canvas.
+  useEffect(() => {
+    const local = new fabric.Canvas('c')
+    updateCanvas(local);
+  }, [])
 
   return (
     <div>
       ImageCanvas
       <img src={uploadImage} alt="" style={{maxWidth: "80%", maxHeight: "50%"}} />
-      
+      <canvas id="c"></canvas>
     </div>
   );
 }
