a3dda21e5b175e22d610f368739b3ccd6a8ee0d5
unknown <a-kaname@weldh.local>
2020-07-03 11:15:26 +0900

    React-Konva 導入

--- src/ImageCanvas.js
+++ src/ImageCanvas.js
@@ -1,117 +1,140 @@
-import React, {/* useEffect, useRef, useState */} from "react";
-import Konva from 'konva';
-import { Stage, Layer, Rect, Text, Circle, Line, Image } from "react-konva";
-import useImage from 'use-image';
+import React, { /* useEffect, */ useRef, useState, useEffect } from "react";
+// import Konva from 'konva';
+import {
+  Stage,
+  Layer,
+  Text,
+  Image,
+  Group,
+  Transformer,
+} from "react-konva";
+import useImage from "use-image";
 
-function ComplexApp(imageFile) {
-  const url = imageFile.imageFile.src
-  // set crossOrigin of image as second argument
-  const [image] = useImage(url, 'Anonymous');
-  // const [image, status] = useImage(url, 'Anonymous');
+const URLImage = (props) => {
+  const [image, status] = useImage(props.url, "Anonymous");
+  // const [isDragging, setIsDragging] = useState(false);
+  // const [x, setX] = useState(50);
+  // const [y, setY] = useState(50);
 
   // status can be "loading", "loaded" or "failed"
-  // console.log(url, status)
+  const draggableProps = props.draggable
+    ? {
+        draggable: true,
+        x: props.x,
+        y: props.y,
+        onDragStart: () => {
+          // setIsDragging(true)
+        },
+        onDragEnd: (e) => {
+          // setIsDragging(false)
+          props.setX(e.target.x());
+          props.setY(e.target.y());
+        },
+      }
+    : null;
+
+  console.log(props.url, status);
+  return <Image {...draggableProps} image={image} width={props.width} height={props.height} />;
+};
+
+const BoardGroup = (props) => {
+  const [isSelected, toggleSelect] = useState(false);
+  const groupRef = useRef();
+  const trRef = useRef();
+
+  useEffect(() => {
+    console.log(isSelected);
+    if (isSelected) {
+      // we need to attach transformer manually
+      console.log(trRef, groupRef);
+      trRef.current.setNode(groupRef.current);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
 
   return (
-    <Image image={image} />
+    <>
+      <Group
+        ref={groupRef}
+        draggable={true}
+        x={props.boardX}
+        setX={props.setBoardX}
+        y={props.boardY}
+        setY={props.setBoardY}
+        onmouseover={() => {
+          document.body.style.cursor = "pointer";
+        }}
+        onmouseout={() => {
+          document.body.style.cursor = "default";
+        }}
+        onClick={() => {
+          toggleSelect(!isSelected);
+        }}
+      >
+        <URLImage url={props.url} width={640} height={480} />
+        {props.board.inputs.map((input, i) => {
+          return (
+            <Text
+              key={`${props.board.id}-${i}`}
+              text={input.value}
+              fontSize={60}
+              x={input.offsetX}
+              y={input.offsetY}
+            />
+          );
+        })}
+      </Group>
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          rotateEnabled={false}
+          keepRatio={true}
+          enabledAnchors={[
+            "top-left",
+            "top-right",
+            "bottom-left",
+            "bottom-right",
+          ]}
+        />
+      )}
+    </>
   );
-}
-
-// function URLImage(props) {
-
-//   const imageRef = useRef(null);
-//   const [image, setImage] = useState(null);
-
-//   useEffect(() => {
-//     loadImage();
-//     return () => {
-//       if(imageRef.current) {
-//         imageRef.current.removeEventListener('load', handleLoad);
-//       }
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     loadImage();
-//   }, [props.src])
-
-//   function handleLoad() {
-//     setImage(imageRef.current);
-//   }
-
-//   function loadImage() {
-//     const img = new window.Image();
-//     img.src = props.src;
-//     img.crossOrigin="Anonymous";
-//     imageRef.current = img;
-//     imageRef.current.addEventListener('load', handleLoad);
-//   }
-
-//   return (
-//     <image image={image}/>
-//   );
-// }
+};
 
 const ImageCanvas = (props) => {
   // const { imageFile, canvas, updateCanvas } = props;
-  const { imageFile } = props;
+  const { imageFile, boardFormat } = props;
 
-  // const local = new fabric.Canvas("c");
-  // // canvas.
-  // useEffect(() => {
-  //   const local = new fabric.Canvas("c");
-  //   updateCanvas(local);
-  // }, []);
-  // const setCanvas = (src) => {
-  //   local.setBackgroundImage(src, local.renderAll.bind(local));
-  // };
+  const board = boardFormat.filter((board) => board.active)[0];
+  const svgString = board.svgCode;
+  const url = "data:image/svg+xml;base64," + window.btoa(svgString);
 
-  // useEffect(() => {
-  //   if (imageFile.src) {
-  //     // console.log(local)
-  //     console.log(
-  //       `Canvas Update. src: ${imageFile.src}, width: ${imageFile.width}, height: ${imageFile.height}`
-  //     );
-  //     local.setBackgroundImage(imageFile.src, local.renderAll.bind(local));
-  //     local.setHeight(imageFile.height);
-  //     local.setWidth(imageFile.width);
-  //     // setCanvas(imageFile.src)
-  //   }
-  //   // document.title = `You clicked ${count} times`;
-  // });
+  const [boardX, setBoardX] = useState(50);
+  const [boardY, setBoardY] = useState(50);
 
   return (
     <div>
       <h2>ImageCanvas</h2>
       <div>
-        <Stage width={imageFile.width||300} height={imageFile.height||300}>
+        <Stage width={imageFile.width || 300} height={imageFile.height || 300}>
           <Layer>
-            <ComplexApp imageFile={imageFile} />
-            <Text text="Some text on canvas" fontSize={15} />
-            <Rect
-              x={20}
-              y={50}
-              width={100}
-              height={100}
-              fill="red"
-              shadowBlur={10}
-            />
-            <Circle x={200} y={100} radius={50} fill="green" />
-            <Line
-              x={20}
-              y={200}
-              points={[0, 0, 100, 0, 100, 100]}
-              tension={0.5}
-              closed
-              stroke="black"
-              fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-              fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-              fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
-            />
+            {imageFile.src && <URLImage url={imageFile.src} />}
+            {imageFile.src && (
+              <BoardGroup
+                {...{
+                  board,
+                  url,
+                  boardX,
+                  setBoardX,
+                  boardY,
+                  setBoardY,
+                }}
+              />
+            )}
           </Layer>
         </Stage>
       </div>
-      {imageFile.src ? (
+      {/* {imageFile.src ? (
         <>
           <h3>Preview</h3>
           <div>
@@ -123,7 +146,7 @@ const ImageCanvas = (props) => {
             />
           </div>
         </>
-      ) : null}
+      ) : null} */}
     </div>
   );
 };
